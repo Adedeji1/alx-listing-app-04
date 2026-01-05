50636972fde230db68788fa285f81d86ee3d7fae
Adedeji1 <afolabiadedeji110@yahoo.com>
2026-01-05 23:19:11 +0100

    worked with Axios for API consumption

---
+++ interfaces/index.ts
@@ -0,0 +1,26 @@
+export interface PropertyProps {
+  name: string;
+  address: {
+    state: string;
+    city: string;
+    country: string;
+  };
+  rating: number;
+  category: string[];
+  price: number;
+  offers: {
+    bed: string;
+    shower: string;
+    occupants: string;
+  };
+  image: string;
+  discount: string;
+}
+export interface Property {
+  id: string;
+  name: string;
+  image: string;
+  price: number;
+  rating: number;
+  location?: string;
+}
